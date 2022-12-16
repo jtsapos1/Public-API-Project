285a7b5f0f37cdf176db858ef519f09510c3b7b8
jtsapos <jtsapos@gmx.com>
2022-12-16 15:27:31 -0500

    Updated js\app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ fetch(urlAPI)
 .catch(err => console.log(err))
 
 function displayEmployees(employeeData) {
-
     employees = employeeData;
 
     // store the employee HTML as we create it
@@ -42,39 +41,49 @@ employeeHTML += `
 });
 
 gridContainer.innerHTML = employeeHTML;
-}
+};
+
+/////DISPLAY MODAL//////
 
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
-    let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employees[index];
-    let date = new Date(dob.date);
+    let { name, 
+        dob, 
+        phone, 
+        email, 
+        location: { city, street, state, postcode }, picture } = employees[index];
+    
+		let date = new Date(dob.date);
+
     const modalHTML = `
     <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
+    <div class="modal-text-container">
     <h2 class="name">${name.first} ${name.last}</h2>
     <p class="email">${email}</p>
     <p class="address">${city}</p>
     <hr />
-    <p>${phone}</p>
-    <p class="address">${street}, ${state} ${postcode}</p>
-    <p>Birthday:
+    <p class="phone">${phone}</p>
+    <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+    <p class="birthday">Birthday:
     ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
     `;
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-    }
+    };
+
     gridContainer.addEventListener('click', e => {
         // make sure the click is not on the gridContainer itself
         if (e.target !== gridContainer) {
         // select the card element based on its proximity to actual element
-        clicked
+        //clicked
         const card = e.target.closest(".card");
         const index = card.getAttribute('data-index');
         displayModal(index);
         }
         });
+
+				///CLOSE MODAL////
 
         modalClose.addEventListener('click', () => {
             overlay.classList.add("hidden");
